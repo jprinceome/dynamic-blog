c2940dcccd7f2faa645eb4e41328b64052bde0ed
jprinceome <ome2john@gmail.com>
2022-01-24 02:49:01 +0100

    Initial commit

---
+++ Blog/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+from .feeds import LatestPostsFeed
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path("feed/rss", LatestPostsFeed(), name="post_feed"),
+    
+]
